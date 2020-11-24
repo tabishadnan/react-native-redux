7f37014a65d3901b6a320f1ea3f4b516c6cd8e04
tabishadnan <tabishadnan000@gmail.com>
2020-11-24 22:48:06 +0500

    fix redux actions and reducers

--- components/CounterComponent.js
+++ components/CounterComponent.js
@@ -1,15 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { useSelector } from 'react-redux';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
 
 const CounterComponent = () => {
 
   const counter = useSelector(state => state.CounterReducer.counter);
-  
+
+  const dispatch = useDispatch();
+
   return (
     <View style={styles.container}>
       <Text>Counter : {counter}</Text>
+      <Button title="INCREMENT" onPress={() => dispatch({type:"INCREMENT"})} /> 
       <StatusBar style="auto" />
     </View>
   );
